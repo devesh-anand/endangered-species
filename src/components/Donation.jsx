6f6692f90077139f94571f0e2838b84a806a94bf
scisaif <saiflll284@gmail.com>
2021-10-09 00:54:45 +0530

    donation styles final

--- src/components/Donation.jsx
+++ src/components/Donation.jsx
@@ -4,19 +4,16 @@ import svg1 from "./images/appreciation.svg";
 
 function Donation() {
 	return (
-		<div className="donation-main">
-			<div class="card mb-3" id="he" style={{ width: "50%" }}>
-				<div class="row g-0 ">
-					<div class="col-md-3 my-6" id="svg1-wrapper">
-						<div>
-							<img id="svg1" src={svg1} class="img-fluid rounded-start" alt="..." />
-						</div>
+		<>
+			<div className="donation-main">
+				<div id="donation-card">
+					<div id="svg1-wrapper">
+						<img id="svg1" src={svg1} alt="..." />
 					</div>
-					<div class="col-md-2" id="hello">
-						<div class="card-body">
-							<h5 class="card-title" style={{ fontWeight: "800", fontSize: "2rem" }}>
-								Donate Now
-							</h5>
+
+					<div id="donation-form">
+						<div>
+							<h5 style={{ fontWeight: "800", fontSize: "2rem" }}>Donate Now</h5>
 							<div class="form-check">
 								<input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
 								<label class="form-check-label" for="flexRadioDefault1">
@@ -49,19 +46,19 @@ function Donation() {
 								<input type="text" class="form-control" id="amount" placeholder="Other Amount" aria-label="Username" aria-describedby="addon-wrapping" />
 							</div>
 							<p class="card-text">CHOOSE PAYMENT </p>
-							<button type="button" class="btn mx-4">
-								VISA
-							</button>
-							<button type="button" class=" mx-4 btn my-2">
-								PAYPAL
-							</button>
+							<div className="pay">
+								<button type="button" class="btn ">
+									VISA
+								</button>
+								<button type="button" class="btn">
+									PAYPAL
+								</button>
+							</div>
 						</div>
 					</div>
-						
 				</div>
-                
 			</div>
-		</div>
+		</>
 	);
 }
 
