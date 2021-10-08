89c9d54c23eef0de52be74ed30210dd97ae02dd3
dev <devyanknagpal2002@gmail.com>
2021-10-08 17:19:51 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Slider from './components/slider';
 import Navbar from './components/Navbar.jsx';
+import Donation from "./components/Donation";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Slider />
-
+      <Donation/>
     </div>
   );
 }
