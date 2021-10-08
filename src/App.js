dff748eba19e76433eb5e55b440755edd8c60c0e
scisaif <saiflll284@gmail.com>
2021-10-08 17:17:52 +0530

    added rank component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Navbar from './components/Navbar.jsx';
+import Slider from './components/slider';
+import Rank from './components/Rank';
+
+
+function App() {
+
+	return (
+		<>
+			<Navbar/>
+			<Slider/>
+			{/* <Rank/> */}
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Rank.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './rank.css';
+import bg from './images/rank.svg';
+
+const Rank = () => {
+    return (
+        <>
+            <div className="rank-body">
+                <h2>The 10 most Endangered species in India</h2>
+                {/* <img src={bg} alt="bg" /> */}
+            </div>
+        </>
+    )
+}
+
+export default Rank
