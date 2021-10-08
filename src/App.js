b53e8e1fb5162bf8ae8a0eda69543a80698620a3
Devesh Anand Srivastava <contactdeveshanand@gmail.com>
2021-10-09 01:42:14 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./App.css";
 import Navbar from './components/Navbar.jsx';
 import Slider from './components/slider';
 import Rank from './components/Rank';
+import Donation from './components/Donation';
 
 
 function App() {
-
 	return (
 		<>
+			<Router>
 			<Navbar/>
-			<Slider/>
-			<Rank/>
+				<Switch>
+					<Route path="/" exact component={Slider} />
+					<Route exact path="/rank" component={Rank} />
+					<Route exact path="/donate" component={Donation} />
+				</Switch>
+			</Router>
 		</>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark">
 				<div className="container-fluid">
-					<a className="navbar-brand" href="#">
+					<a className="navbar-brand" href="/">
 						Endangered Species
 					</a>
 					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,18 +18,18 @@ const Navbar = () => {
 					<div className="collapse navbar-collapse" id="navbarNav">
 						<ul className="navbar-nav">
 							<li className="nav-item">
-								<a className="nav-link active" id="home" aria-current="page" href="#">
+								<a className="nav-link active" id="home" aria-current="page" href="/">
 									Home
 								</a>
 							</li>
 							<li className="nav-item">
-								<a className="nav-link" id="donate" href="#">
-									Donate
+								<a className="nav-link" id="rank" href="/rank">
+									Rank
 								</a>
 							</li>
 							<li className="nav-item">
-								<a className="nav-link" id="rank" href="#">
-									Rank
+								<a className="nav-link" id="donate" href="/donate">
+									Donate
 								</a>
 							</li>
 						</ul>
