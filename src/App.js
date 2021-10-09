58948aefa8154c83959f0401dfa9afba6aa37b74
dev <devyanknagpal2002@gmail.com>
2021-10-09 15:00:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from './components/Navbar.jsx';
 import Slider from './components/slider';
 import Rank from './components/Rank';
+import About from "./components/About";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 			<Navbar/>
 			<Slider/>
 			<Rank/>
+			<About/>
 		</>
 	);
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './about.css';
+import about from './about.png'
+// import bg from './Cessation.svg';
+
+export default function About() {
+    return (
+        <div class="col">
+            <section id="About">
+        <div class="about-left-col">
+            <img src={about} alt="" />
+        </div>
+        <div class="about-right-col">
+            <div class="about-text">
+                <h1>About Us</h1>
+                <p>this project was made for live the code hakathon
+                    </p><br/>
+                    <h2>OBJECTIVE</h2>
+                    <p>For maintaining a healthy ecological balance on this earth, animals, plants and marine species are as important as humans. Each organism on this earth has a unique place in food chain that helps contribute to the ecosystem in its own special way. But, sadly today, many of the animals and birds are getting endangered</p>
+               {/* <h3>CONTRIBUTORS</h3> */}
+               <p></p>
+                <div class="line">
+                    <span class="line-1"></span><br/>
+                    <span class="line-2"></span><br/>
+                    <span class="line-3"></span>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container">
+    <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
+    <div class="card p-4">
+        <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img  class="red"src="https://lh3.googleusercontent.com/yD_BdtzW62L-1w1igiriy-HVJKb1G0fHD85qfTcqefbqQ7y9sVdpcXLJzrTnEVGoUsfONfPS-ujWnb_vsm7BctcJoUop5dGuZxOY-Exq3ft1Vb_Wz-sgxKGHS5Y-Uw4setIXY-WeCeE=w600-h315-p-k" height="100" width="100" /></button> <span class="name mt-3">Saifullah Rahman</span> 
+            <div class="d-flex flex-row justify-content-center align-items-center gap-2">  </div>
+            <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1069 <span class="follow">Followers</span></span> </div>
+            {/* <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div> */}
+            {/* <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br/><br/> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div> */}
+            <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">  <span><a href="https://github.com/SciSaif" target="_blank"> <i class="fa fa-github"></i></a></span> <span><a href="https://twitter.com/scisaif" target="_blank"> <i class="fa fa-twitter"></i></a></span> <span><a href="https://www.linkedin.com/in/saifullah-rahman-096a68177/"  target="_blank"><i class="fa fa-linkedin"></i></a></span> </div>
+            {/* <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div> */}
+        </div>
+    </div>
+</div>
+
+<div class="container mt-4 mb-8 p-3 d-flex justify-content-center">
+    <div class="card p-4">
+        <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img class="red" src="https://cdn.discordapp.com/attachments/895703526737575966/896310681513627678/IMG_20200810_150531.jpg" height="100" width="100" /></button> <span class="name mt-3">Devesh Anand </span> 
+            <div class="d-flex flex-row justify-content-center align-items-center gap-2">  </div>
+            <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1200 <span class="follow">Followers</span></span> </div>
+            {/* <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div> */}
+            {/* <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br/><br/> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div> */}
+            <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">  <span><a href="https://github.com/devesh-anand" target="_blank"> <i class="fa fa-github"></i></a></span>  <span><a href="https://twitter.com/deveshanand18" target="_blank"> <i class="fa fa-twitter"></i></a></span><span><a href="https://www.linkedin.com/in/deveshanand18/"  target="_blank"><i class="fa fa-linkedin"></i></a></span> </div>
+            {/* <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div> */}
+        </div>
+    </div>
+</div>
+<div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
+    <div class="card p-4">
+        <div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img  class="red"src="https://media-exp1.licdn.com/dms/image/C4D03AQHWiSBT0PsAOQ/profile-displayphoto-shrink_100_100/0/1630004221877?e=1639008000&v=beta&t=Mc6UiQvE321SmXFY0S4e5IB7tJf0igRvZLl9WftUyZE" height="100" width="100" /></button> <span class="name mt-3">Devyank Nagpal</span> 
+            <div class="d-flex flex-row justify-content-center align-items-center gap-2">  </div>
+            <div class="d-flex flex-row justify-content-center align-items-center mt-3"> <span class="number">1022 <span class="follow">Followers</span></span> </div>
+            {/* <div class=" d-flex mt-2"> <button class="btn1 btn-dark">Edit Profile</button> </div> */}
+            {/* <div class="text mt-3"> <span>Eleanor Pena is a creator of minimalistic x bold graphics and digital artwork.<br/><br/> Artist/ Creative Director by Day #NFT minting@ with FND night. </span> </div> */}
+            <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">  <span><a href="https://github.com/2devyank" target="_blank"> <i class="fa fa-github"></i></a></span> <span><a href="https://twitter.com/DevyankNagpal" target="_blank"> <i class="fa fa-twitter"></i></a></span> <span><a href="https://www.linkedin.com/in/devyank-nagpal-58564a204/"  target="_blank"><i class="fa fa-linkedin"></i></a></span> </div>
+            {/* <div class=" px-2 rounded mt-4 date "> <span class="join">Joined May,2021</span> </div> */}
+        </div>
+    </div>
+</div>
+</div>
+
+        </div>
+    )
+}
